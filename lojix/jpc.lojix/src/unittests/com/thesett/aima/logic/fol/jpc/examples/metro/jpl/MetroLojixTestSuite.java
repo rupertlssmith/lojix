9703d062b8da43f009b8978decbc180999a2a3d4
Rupert Smith <github@badgerhouse.freeserve.co.uk>
2014-07-14 11:44:07 +0100

    Cleaned out comments.

--- lojix/jpc.lojix/src/unittests/com/thesett/aima/logic/fol/jpc/examples/metro/jpl/MetroLojixTestSuite.java
+++ lojix/jpc.lojix/src/unittests/com/thesett/aima/logic/fol/jpc/examples/metro/jpl/MetroLojixTestSuite.java
@@ -21,12 +21,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/**
- * This test suit exists only for testing individually the metro example for the jpl implementation. It will be deleted
- * soon...
- *
- * @author sergioc
- */
 @RunWith(Suite.class)
 @SuiteClasses({ MetroTestSuite.class })
 public class MetroLojixTestSuite
--- lojix/jpc.lojix/src/unittests/com/thesett/aima/logic/fol/jpc/jpl/LojixPrologEngineTestSuite.java
+++ lojix/jpc.lojix/src/unittests/com/thesett/aima/logic/fol/jpc/jpl/LojixPrologEngineTestSuite.java
@@ -24,11 +24,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/**
- * All the non-Logtalk tests the logic engine should pass
- *
- * @author sergioc
- */
 @RunWith(Suite.class)
 @SuiteClasses({ PrologEngineTestSuite.class, PrologExamplesTestSuite.class, LojixTransformationTest.class })
 public class LojixPrologEngineTestSuite
